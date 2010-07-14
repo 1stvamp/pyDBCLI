4386c117e6a80350c036d931b636b0b6d4a513a5
Wesley Mason <wes [at] 1stvamp.org>
2010-07-14 15:35:48 +0100

    Added installer

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""Installer for pyDBCLI"""
+
+try:
+        from setuptools import setup, find_packages
+except ImportError:
+        from ez_setup import use_setuptools
+        use_setuptools()
+        from setuptools import setup, find_packages
+setup(
+    name='pyDBCLI',
+    description='Python database CLI lib',
+    version='0.1',
+    author='Wes Mason',
+    author_email='wes[at]1stvamp[dot]org',
+    url='http://github.com/1stvamp/pyDBCLI',
+    packages=find_packages(exclude=['ez_setup'])
+)
